1a41cec3a9dab630915306c0bd5666b01250219a
JMinformaticajorge <126088128+JMinformaticajorge@users.noreply.github.com>
2024-03-06 21:12:35 -0300

    Add files via upload

---
+++ int_js_3d/1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,16 @@
+//tipos number
+
+const meuNumero = 24;
+
+const telefone = 4464581605;
+
+const idade = 17;
+
+
+console.log(meuNumero);
+console.log(telefone);
+console.log(idade);
+
+const soma = meuNumero + idade;
+
+console.log(soma);
---
+++ int_js_3d/script.js
@@ -0,0 +1,14 @@
+const texto1 = "kaike cupin";
+const texto2 = 'gabzin';
+
+const texto3 = ' foi no "banheiro"';
+
+
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+
+console.log(texto2 + texto3);
+
+
+
