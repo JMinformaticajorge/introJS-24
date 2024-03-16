858550a20d1acf9dae5405e4acfbbaa9cdb2badd
JMinformaticajorge <126088128+JMinformaticajorge@users.noreply.github.com>
2024-03-15 22:11:14 -0300

    Add files via upload

---
+++ 1_tipos_primitivos/3-boolean/script.js
@@ -0,0 +1,21 @@
+const texto1 = "ola mundo"
+const texto2 = 'ola mundo'
+const senha = "senhasupersegura"
+const StringDeNumeros = "3644"
+
+//concatenacao (+)
+
+
+const citacao = "meu nome é"
+const meuNome = "jorge leal"
+console.log(citacao + meuNome)
+
+//comparacao
+
+console.log(texto1 === texto2)
+
+const n1 = 1
+const n2 = 2
+
+console.log(n1 === n2)
+
